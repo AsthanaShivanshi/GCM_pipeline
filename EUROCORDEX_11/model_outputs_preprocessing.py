7c2807a7723e6df8ea9f23e69fcd96d9538d216d
sasthana <shivanshi.asthana@unil.ch>
2025-11-25 13:55:01 +0100

    Changes

--- EUROCORDEX_11/model_outputs_preprocessing.py
+++ EUROCORDEX_11/model_outputs_preprocessing.py
@@ -1,10 +1,10 @@
 import os
 import xarray as xr
-import config
+import numpy as np
+from scipy.interpolate import griddata
 
-# Swiss bounding box
+# Swiss bounding box (lon_min, lon_max, lat_min, lat_max)
 CH_BOX = (5, 11, 45, 48)
-
 
 def masking(input_path, mask_path, output_folder, varname):
     fname = os.path.basename(input_path)
@@ -15,32 +15,48 @@ def masking(input_path, mask_path, output_folder, varname):
         return
 
     with xr.open_dataset(input_path) as ds:
-        lat2d = ds["lat"]
-        lon2d = ds["lon"]
-        mask_box = (
+        lat2d = ds["lat"].values
+        lon2d = ds["lon"].values
+
+        # Swiss boiundign box
+        swiss_mask = (
             (lon2d >= CH_BOX[0]) & (lon2d <= CH_BOX[1]) &
             (lat2d >= CH_BOX[2]) & (lat2d <= CH_BOX[3])
         )
-        ds[varname] = ds[varname].where(mask_box)
+
+        # Crop rlat/rlon to Swiss domain
+        rlat_idx, rlon_idx = np.where(swiss_mask)
+        rlat_sel = np.unique(ds['rlat'].values[rlat_idx])
+        rlon_sel = np.unique(ds['rlon'].values[rlon_idx])
+        ds = ds.sel(rlat=rlat_sel, rlon=rlon_sel)
+        lat2d = ds["lat"].values
+        lon2d = ds["lon"].values
 
         with xr.open_dataset(mask_path) as mask_ds:
-            mask = mask_ds["mask"]
-            mask_aligned = mask.reindex_like(ds[varname].isel(time=0), method="nearest")
+            mask = mask_ds["mask"].values
+            mask_lat = mask_ds["lat"].values
+            mask_lon = mask_ds["lon"].values
 
-        if "time" in ds[varname].dims and "time" not in mask_aligned.dims:
-            mask_broadcast = mask_aligned.expand_dims({"time": ds[varname].coords["time"]}, axis=0)
+            # Flatten mask grid for interpolation
+            points = np.column_stack([mask_lat.ravel(), mask_lon.ravel()])
+            values = mask.ravel()
+
+            # Interpolate mask to cropped data grid
+            mask_on_data_grid = griddata(points, values, (lat2d, lon2d), method='nearest')
+
+        # Broadcast mask to match data shape (time, rlat, rlon)
+        if "time" in ds[varname].dims:
+            mask_broadcast = np.broadcast_to(mask_on_data_grid, ds[varname].shape)
         else:
-            mask_broadcast = mask_aligned
+            mask_broadcast = mask_on_data_grid
 
+        # Apply mask
         ds[varname] = ds[varname].where(mask_broadcast)
-
-        valid_rlat = ds["rlat"].where(mask_aligned.sum(dim="rlon") > 0, drop=True)
-        valid_rlon = ds["rlon"].where(mask_aligned.sum(dim="rlat") > 0, drop=True)
-        ds = ds.sel(rlat=valid_rlat, rlon=valid_rlon)
 
         os.makedirs(output_folder, exist_ok=True)
         ds.to_netcdf(outname, mode="w")
 
+import config
 
 VAR_CONFIG = {
     "pr": {
@@ -64,9 +80,6 @@ VAR_CONFIG = {
         "output_folder": os.path.join(config.EUROCORDEX_11_DIR, "tasmin_Swiss"),
     },
 }
-
-
-
 
 for varname, cfg in VAR_CONFIG.items():
     os.makedirs(cfg["output_folder"], exist_ok=True)
