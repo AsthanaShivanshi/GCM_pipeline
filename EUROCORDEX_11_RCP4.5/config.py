3d9e8a0406dc4ec86688343418b5fcb07b53a901
sasthana <shivanshi.asthana@unil.ch>
2025-12-28 23:54:37 +0100

    Changes

---
+++ EUROCORDEX_11_RCP4.5/config.py
@@ -0,0 +1,11 @@
+import os
+BASE_DIR = os.environ.get("BASE_DIR", "/work/FAC/FGSE/IDYST/tbeucler/downscaling")
+DATASETS_TRAINING_DIR= os.environ.get("DATASETS_TRAINING_DIR", f"{BASE_DIR}/sasthana/Downscaling/Downscaling_Models/Dataset_Setup_I_Chronological_12km")
+UNET_1971_DIR= os.environ.get("UNET_1971", f"{BASE_DIR}/sasthana/Downscaling/Downscaling_Models/Dataset_Setup_I_Chronological_12km")
+TARGET_DIR= os.environ.get("TARGET_DIR", f"{BASE_DIR}/sasthana/Downscaling/Processing_and_Analysis_Scripts/data_1971_2023/HR_files_full")   
+MODELS_RUNS_EUROCORDEX_11_RCP85= os.environ.get("MODELS_RUNS_EUROCORDEX_11_RCP85", f"{BASE_DIR}/sasthana/Downscaling/GCM_pipeline/EUROCORDEX_11_RCP8.5")
+MODEL_RUNS_EUROCORDEX_11_RCP45= os.environ.get("MODEL_RUNS_EUROCORDEX_11_RCP45", f"{BASE_DIR}/sasthana/Downscaling/GCM_pipeline/EUROCORDEX_11_RCP4.5")
+MODEL_RUNS_EUROCORDEX_11_RCP26= os.environ.get("MODEL_RUNS_EUROCORDEX_11_RCP26", f"{BASE_DIR}/sasthana/Downscaling/GCM_pipeline/EUROCORDEX_11_RCP2.6")
+OUTPUTS_MODELS_DIR= os.environ.get("OUTPUTS_MODELS_DIR", f"{BASE_DIR}/sasthana/Downscaling/Downscaling_Models/Outputs")
+BC_SCRIPTS= os.environ.get("BC_SCRIPTS", f"{BASE_DIR}/sasthana/Downscaling/GCM_pipeline/BC_methods")
+SCRATCH_DIR= os.environ.get("SCRATCH_DIR", f"/scratch/sasthana")
--- preprocessing_EUR_11.sh
+++ preprocessing_EUR_11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=EUR_11_Preprocessing_pr_rcp85
-#SBATCH --output=logs/preprocess_pr_rcp85_job_output-%j.txt
-#SBATCH --error=logs/preprocess_pr_rcp85_job_error-%j.txt
+#SBATCH --job-name=EUR_11_Preprocessing_tasmax_rcp45
+#SBATCH --output=logs/preprocess_tasmax_rcp45_job_output-%j.txt
+#SBATCH --error=logs/preprocess_tasmax_rcp45_job_error-%j.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=15:00:00
@@ -15,4 +15,4 @@ export PYTHONPATH="$PROJECT_DIR"
 mkdir -p logs
 
 cd "$BASE_DIR/sasthana/Downscaling/GCM_pipeline"
-python EUROCORDEX_11_RCP8.5/model_runs_preprocessing.py
+python EUROCORDEX_11_RCP4.5/model_runs_preprocessing.py
